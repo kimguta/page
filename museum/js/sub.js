1d8ff273382ff13440ce983445d60defefb0d386
kimguta <70299722+kimguta@users.noreply.github.com>
2020-10-16 16:04:09 +0900

    Add files via upload

--- museum/js/sub.js
+++ museum/js/sub.js
@@ -8,4 +8,17 @@ $(function() {
 		$('#content .share .open').focus();
 	});
 
+	$('.calendar01 td.possible > a').on('click', function (e) {
+		e.preventDefault();
+		if ($(this).parent().hasClass('active')) {
+			$(this).parent().removeClass('active');
+			$(this).parents('tr').next('.schedule').fadeOut(300);
+		} else{
+			$('.calendar01 td').removeClass('active');
+			$(this).parent().addClass('active');
+			$('.calendar01 .schedule').fadeOut(300);
+			$(this).parents('tr').next('.schedule').fadeIn(300);
+		}
+	});
+
 });
