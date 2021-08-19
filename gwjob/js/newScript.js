9dadcd68dc0402e57aa3ef6512fe64a8e5b32856
kimguta <70299722+kimguta@users.noreply.github.com>
2021-08-19 17:09:51 +0900

    Add files via upload

--- gwjob/js/newScript.js
+++ gwjob/js/newScript.js
@@ -194,6 +194,13 @@ $(function() {
 	});
 
 
+	var currentPosition2 = parseInt($("#side-btn").css("top"));
+	$(window).on('scroll', function () {
+		var posY2 = $(window).scrollTop();
+		$("#side-btn").stop().animate({"top":posY2+currentPosition2+"px"},500);
+	});
+
+
 	$('#footer .open').on('click', function (e) {
 		e.preventDefault();
 		if ($(this).hasClass('active')) {
@@ -351,7 +358,8 @@ $('.edu.view .status button').on('click', function(e) {
 if (windowWidth >= 768) {
     $('.select-content').css('height', $('.selector').height());
 }
-/*$("input.date").datepicker({
+
+$("input.date").datepicker({
     dateFormat: 'yy-mm-dd', //Input Display Format 변경
     showOtherMonths: true, //빈 공간에 현재월의 앞뒤월의 날짜를 표시
     showMonthAfterYear: true, //년도 먼저 나오고, 뒤에 월 표시
@@ -365,7 +373,7 @@ if (windowWidth >= 768) {
     monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'], //달력의 월 부분 Tooltip 텍스트
     dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'], //달력의 요일 부분 텍스트
     dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일']
-});*/
+});
 
 /* 조직도 */
 $('.org h2 button').on('click', function(e) {
