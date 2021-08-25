63e1816aa05cb04de32595b1632ee443f1e75201
kimguta <70299722+kimguta@users.noreply.github.com>
2021-08-25 11:05:25 +0900

    Add files via upload

---
+++ geopark/js/script.js
@@ -0,0 +1,191 @@
+$(function() {
+    
+    var navOffset = $('#header').offset();
+
+	$(window).on('scroll load', function () {
+	    if ($(document).scrollTop() > 30) {
+	        $('#header').addClass('fixed');
+	    } else {
+	        $('#header').removeClass('fixed');
+	    }
+	});
+
+
+
+	$('#gnb_pc .depth_01 li').on('mouseover focusin', function () {
+		var offset = $(this).position().left + ($(this).width() / 2 - 16);
+		$('#header').addClass('active');
+		$('#gnb_pc h2').removeClass('active');
+		$(this).children('h2').addClass('active');
+		$('#gnb_pc .depth_02').stop().fadeOut(50);
+		$(this).children('.depth_02').stop().fadeIn(50);
+		$('.bg_pc').show().stop().animate({height:385},300);
+		$('#gnb_pc .cycle').stop().animate({'left':offset}, 600,'easeOutCubic');
+		$('.mask_pc').show();
+	});
+
+	$('#header #gnb_pc h3 a').on('click', function (e) {
+		e.preventDefault();
+		if ($(this).parent().hasClass('active')) {
+			$(this).parent().removeClass('active');
+			$(this).parent().next('.depth_03').slideUp(300);
+		} else{
+			$('#header #gnb_pc h3').removeClass('active');
+			$('#header #gnb_pc .depth_03').slideUp(300);
+			$(this).parent().addClass('active');
+			$(this).parent().next('.depth_03').slideDown(300);
+		}
+	});
+
+	$('#header').on('mouseleave', function () {
+		$('#header').removeClass('active');
+		$('#gnb_pc h2').removeClass('active');
+		$('#gnb_pc .depth_02').stop().fadeOut(50);
+		$('.bg_pc').stop().animate({height:0},200);
+		$('.mask_pc').hide();
+	});	
+	
+
+	$('#gnb_pc .depth_02 li:last-child a').on('focusout', function () {
+		$('#header').removeClass('active');
+		$('#gnb_pc h2').removeClass('active');
+		$('#gnb_pc .depth_02').stop().fadeOut(50);
+		$('.bg_pc').stop().animate({height:0},200);
+		$('.mask_pc').hide();
+	});
+
+
+	$('#header .mopen').on('click', function (e) {
+		e.preventDefault();
+	    $(this).toggleClass('active');
+		$('#gnb_mobile').toggleClass('active');
+		$('.mask_mobile').fadeToggle(200);
+		if ($(this).hasClass('active')) {
+			$('body').addClass('fixed');
+		} else{
+			$('body').removeClass('fixed');
+		}
+	});
+
+	$('.mask_mobile').on('click', function (e) {
+		e.preventDefault();
+		$('#gnb_mobile').removeClass('active');
+		$('#header .mopen').removeClass('active');
+		$('.mask_mobile').fadeOut(200);
+		$('body').removeClass('fixed');
+	});
+
+	$('#gnb_mobile').swipe({
+        swipeStatus:function(event, phase, direction, distance, duration, fingers, fingerData) {
+			if( direction == "right"){	
+				if(distance > 150){
+					$('.mask_mobile').hide();
+					$('#gnb_mobile').removeClass('active');
+					$('#gnb_mobile').removeAttr('style');
+					$('body').removeClass('fixed');
+					$('#header .mopen').removeClass('active');
+				}else{
+					if (phase=="move"){
+						$('#gnb_mobile').css('right',-distance); 
+					}
+					if (phase=="end"){
+						$('#gnb_mobile').removeAttr('style');
+					}
+				}
+			}
+        },
+		allowPageScroll:"vertical",
+        threshold:0,
+		excludedElements: "a, label, button, input, select, textarea, .slick"
+    });
+
+
+
+	$('#gnb_mobile .depth_01 h2.active').next('.depth_02').show();
+    $('#gnb_mobile .depth_02 h3.active').next('.depth_03').show();
+
+	$('#gnb_mobile h2 a').on('click', function (e) {
+        e.preventDefault();
+		if ($(this).parent().hasClass('active')) {
+			$(this).parent().removeClass('active');
+			$(this).parent().next('.depth_02').slideUp(300);
+		} else{
+            $('#gnb_mobile h2').removeClass('active');
+            $('#gnb_mobile .depth_02').slideUp(300);
+            $(this).parent().addClass('active');
+            $(this).parent().next('.depth_02').slideDown(300);
+		} 
+	});
+
+
+	$('#gnb_mobile .depth_03').prev('h3').addClass('has_depth');
+	
+    $('#gnb_mobile .depth_02 h3.has_depth a').on('click', function (e) {
+		e.preventDefault();
+		if ($(this).parent().hasClass('active')) {
+			$(this).parent().removeClass('active');
+			$(this).parent().next('.depth_03').slideUp(300);
+		} else{
+			$('#gnb_mobile h3').removeClass('active');
+			$('#gnb_mobile .depth_03').slideUp(300);
+			$(this).parent().addClass('active');
+			$(this).parent().next('.depth_03').slideDown(300);
+		}
+	});
+
+	$('#header .btn_sitemap').on('click', function (e) {
+		e.preventDefault();
+	    $('#sitemap').show();
+		$('html, body').animate({scrollTop: 0}, 400);
+	});
+
+	$('#sitemap .close').on('click', function (e) {
+		e.preventDefault();
+	    $('#sitemap').hide();
+	});
+
+	$('#btn_top').on('click', function (e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop: 0}, 400);
+	}); 
+
+	var currentPosition = parseInt($("#btn_top").css("top"));
+	$(window).on('scroll', function () {
+		var posY = $(window).scrollTop();
+		$("#btn_top").stop().animate({"top":posY+currentPosition+"px"},500);
+		if ( posY > 100 ){
+			$("#btn_top").css('opacity','.8');
+		} else if(posY < 100) {
+			$("#btn_top").css('opacity','0');
+		};
+	});
+
+
+	$('#footer .open').on('click', function (e) {
+		e.preventDefault();
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+		} else{
+			$('#footer .open').removeClass('active');
+			$(this).addClass('active');
+		} 
+	});
+
+
+	$('#footer .view a:last-child').on('focusout', function () {
+		$('#footer .open').removeClass('active');
+		$(this).parent('.view').prev('.open').focus();
+	});
+
+	$('#footer .family').on('mouseleave', function () {
+		$('#footer .open').removeClass('active');
+	});	
+
+	$('a[role="button"]').on('keypress', function (key) {
+		if (key.keyCode == 32) {
+			$(this).trigger('click');
+			return false
+		}
+	});
+
+});
---
+++ geopark/js/sub.js
@@ -0,0 +1,45 @@
+$(function() {
+
+	 $('#visual-bx .share .open').on('click', function (e) {
+		 e.preventDefault();
+		$(this).next('.view').fadeToggle(100);
+	});
+
+	$('#visual-bx .share .close').on('click', function (e) {
+		e.preventDefault();
+	   $(this).parents('.view').fadeToggle(100);
+   });
+
+	$('#content .share .view a:last').on('focusout', function () {
+		$('#content .share .open').focus();
+		$('.view').fadeOut(100);
+	});
+
+	$('#breadcrumb .open').on('click', function (e) {
+		e.preventDefault();
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			$(this).children('span').text('열기');
+			$('#breadcrumb .box ul').slideUp(300);
+		} else{
+			$('#breadcrumb .open').removeClass('active');
+			$(this).addClass('active');
+			$('#breadcrumb .open span').text('열기');
+			$(this).children('span').text('닫기');
+			$('#breadcrumb .box ul').slideUp(300);
+			$(this).next('ul').slideDown(300);
+		}
+	});
+
+	$('#breadcrumb').on('mouseleave', function (e) {
+		e.preventDefault();
+		$('#breadcrumb .open span').text('열기');
+		$('#breadcrumb .box ul').slideUp(300);
+		$('#breadcrumb .box .open').removeClass('active');
+	});
+
+	$('#breadcrumb .box ul li:last-child a').on('focusout', function () {
+		$(this).parents('.box').children('.open').focus();
+	});
+	
+});
