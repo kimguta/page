0a594cfd95efa1ac3f2ddc08b9664d063856eace
kimguta <70299722+kimguta@users.noreply.github.com>
2021-10-08 17:06:32 +0900

    Add files via upload

--- ecology/js/main.js
+++ ecology/js/main.js
@@ -6,129 +6,57 @@ $(function() {
             duration: 1200,
             delay: 200,
             once: true,
-            offset: 150
+            offset: 200
         });
      }, 200);
 
 
- 
     $('#visual .bg-bx > div a').on('mouseenter', function (e) {
         e.preventDefault();
-        $('#visual .bg-bx div').removeClass('bg');
-        $('#visual .bg-bx div.active').addClass('bg').delay(100).removeClass('active');
-        $(this).parent('div').addClass('active');
 
-        var idx = $(this).parent('div').index();
+        if ($(this).parent('div').hasClass('active')) {
+            
+        }
+        else {
+            $('#visual .bg-bx div').removeClass('bg');
+            $('#visual .bg-bx div.active').addClass('bg').delay(100).removeClass('active');
+            $(this).parent('div').addClass('active');
 
-        $('#visual .inner .item').hide();
-        $('#visual .inner .item').eq(idx).fadeIn(1000);
+            var idx = $(this).parent('div').index();
 
+            $('#visual .inner .item').hide();
+            $('#visual .inner .item').eq(idx).fadeIn(1000);
+        }
     });
 
-     /*
-     $('#visual .slick').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-        $('#visual .slick .text').fadeOut(300);
-    }); 
-
-    $('#visual .slick').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-        $('#visual .slick .text').fadeIn(300);
-    });
-
-    $('#visual .slick').slick({
+    $('#vr360 .slick').slick({
         autoplay: true,
         arrows: true,
-        dots: true,
+        dots: false,
+        prevArrow: $('#vr360 .prev'),
+        nextArrow: $('#vr360 .next'),
         accessibility: true,
-        prevArrow: $('#visual .control .prev'),
-        nextArrow: $('#visual .control .next'),
-        appendDots: $('#visual .control .dots'),
-        draggable: true,
-        customPaging: function(slick,index) {
-            return '<a href="#" onclick="return false;">' + (index + 1) + '</a>';
-        },
         infinite: true,
         fade: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         pauseOnHover: false,
-        speed: 1000,
-        autoplaySpeed: 5500
+        speed: 800,
+        autoplaySpeed: 7500
     });
 
 
-    $('#visual .control .bttn').on('click', function (e) {
+    $('#gift .tab a').on('click', function (e) {
         e.preventDefault();
-        if ($(this).hasClass('pause')) {
-            $(this).hide();
-            $('#visual .control .play').css('display','inline-block');
-            $('#visual .slick').slick('slickPause');
-        } else if ($(this).hasClass('play')) {
-            $(this).hide();
-            $('#visual .control .pause').css('display','inline-block');
-            $('#visual .slick').slick('slickPlay');
-        }
+        $('#gift .tab a').removeClass('active');
+        $(this).addClass('active');
+
+        var idx = $(this).index();
+        $('#gift .view-bx > div').hide();
+        $('#gift .view-bx > div').eq(idx).show();
+
     });
 
-
-    $('#notify .slick').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-        var i = (currentSlide ? currentSlide : 0) + 1;
-        $('#notify .control .count').html('<em>' + i + '</em>ㆍ' + slick.slideCount);
-    });
-
-    $('#notify .slick').slick({
-        autoplay: true,
-        arrows: true,
-        dots: false,
-        accessibility: false,
-        prevArrow: $('#notify .prev'),
-        nextArrow: $('#notify .next'),
-        draggable: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        pauseOnHover: false,
-        speed: 900,
-        autoplaySpeed: 4500
-    });
-
-
-    $('#notify .control .bttn').on('click', function (e) {
-        e.preventDefault();
-        if ($(this).hasClass('pause')) {
-            $(this).hide();
-            $('#notify .control .play').css('display','inline-block');
-            $('#notify .slick').slick('slickPause');
-        } else if ($(this).hasClass('play')) {
-            $(this).hide();
-            $('#notify .control .pause').css('display','inline-block');
-            $('#notify .slick').slick('slickPlay');
-        }
-    });
-
-    $('#business .slick').slick({
-        autoplay: false,
-        arrows: true,
-        dots: false,
-        prevArrow: $('#business .prev'),
-        nextArrow: $('#business .next'),
-        accessibility: false,
-        swipeToSlide: true,
-        draggable: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        pauseOnHover: false,
-        speed: 500,
-        responsive: [{
-            breakpoint: 721,
-            settings: {
-                slidesToShow: 2,
-            }
-        }]
-    });
-
-    */
-
-
+ 
 });
 
