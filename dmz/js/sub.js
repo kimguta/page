d4a26ac4e38dac41836023f840ead91e4be53c3b
kimguta <70299722+kimguta@users.noreply.github.com>
2021-10-29 17:52:17 +0900

    Add files via upload

---
+++ dmz/js/sub.js
@@ -0,0 +1,30 @@
+$(function() {
+
+	$('#breadcrumb .open').on('click', function (e) {
+		e.preventDefault();
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			$(this).children('span').text('열기');
+			$('#breadcrumb .box ul').slideUp(300);
+		} else{
+			$('#breadcrumb .open').removeClass('active');
+			$(this).addClass('active');
+			$('#breadcrumb .open span').text('열기');
+			$(this).children('span').text('닫기');
+			$('#breadcrumb .box ul').slideUp(300);
+			$(this).next('ul').slideDown(300);
+		}
+	});
+
+	$('#breadcrumb').on('mouseleave', function (e) {
+		e.preventDefault();
+		$('#breadcrumb .open span').text('열기');
+		$('#breadcrumb .box ul').slideUp(300);
+		$('#breadcrumb .box .open').removeClass('active');
+	});
+
+	$('#breadcrumb .box ul li:last-child a').on('focusout', function () {
+		$(this).parents('.box').children('.open').focus();
+	}); 
+	
+});
