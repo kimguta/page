4cad08e61e321cfddcc09f3bde89db555fc9d3ca
kimguta <70299722+kimguta@users.noreply.github.com>
2021-11-08 16:14:51 +0900

    Add files via upload

--- dmz/js/sub.js
+++ dmz/js/sub.js
@@ -308,4 +308,18 @@ $(function() {
 		}
 	});
 
+	$('.rental .slick').slick({
+		autoplay: false,
+		arrows: true,
+		dots: false,
+		prevArrow: '<a role="button" class="slick-prev prev arrow" href="#">이전</a>',
+        nextArrow: '<a role="button" class="slick-next next arrow" href="#">다음</a>',
+		accessibility: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		pauseOnHover: false,
+		speed: 700,
+	});
+
 });
