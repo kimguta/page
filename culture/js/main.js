a9e1a9020ae2454b9ca91cfead205c0fea808470
임경규 <45418468+paxred@users.noreply.github.com>
2021-11-15 07:14:00 +0900

    Add files via upload

--- culture/js/main.js
+++ culture/js/main.js
@@ -17,110 +17,11 @@ $(function() {
         slidesToShow: 1,
         slidesToScroll: 1
     });
-    
-
-    //  $('#visual .slick').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-    //     $('#visual .slick .text').fadeOut(300);
-    // }); 
-
-    // $('#visual .slick').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-    //     $('#visual .slick .text').fadeIn(300);
-    // });
-
-    // $('#visual .slick').slick({
-    //     autoplay: true,
-    //     arrows: true,
-    //     dots: true,
-    //     accessibility: true,
-    //     prevArrow: $('#visual .control .prev'),
-    //     nextArrow: $('#visual .control .next'),
-    //     appendDots: $('#visual .control .dots'),
-    //     draggable: true,
-    //     customPaging: function(slick,index) {
-    //         return '<a href="#" onclick="return false;">' + (index + 1) + '</a>';
-    //     },
-    //     infinite: true,
-    //     fade: true,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     pauseOnHover: false,
-    //     speed: 1000,
-    //     autoplaySpeed: 5500
-    // });
-
-
-    // $('#visual .control .bttn').on('click', function (e) {
-    //     e.preventDefault();
-    //     if ($(this).hasClass('pause')) {
-    //         $(this).hide();
-    //         $('#visual .control .play').css('display','inline-block');
-    //         $('#visual .slick').slick('slickPause');
-    //     } else if ($(this).hasClass('play')) {
-    //         $(this).hide();
-    //         $('#visual .control .pause').css('display','inline-block');
-    //         $('#visual .slick').slick('slickPlay');
-    //     }
-    // });
-
-
-    // $('#notify .slick').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-    //     var i = (currentSlide ? currentSlide : 0) + 1;
-    //     $('#notify .control .count').html('<em>' + i + '</em>ㆍ' + slick.slideCount);
-    // });
-
-    // $('#notify .slick').slick({
-    //     autoplay: true,
-    //     arrows: true,
-    //     dots: false,
-    //     accessibility: false,
-    //     prevArrow: $('#notify .prev'),
-    //     nextArrow: $('#notify .next'),
-    //     draggable: true,
-    //     infinite: true,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     pauseOnHover: false,
-    //     speed: 900,
-    //     autoplaySpeed: 4500
-    // });
-
-
-    // $('#notify .control .bttn').on('click', function (e) {
-    //     e.preventDefault();
-    //     if ($(this).hasClass('pause')) {
-    //         $(this).hide();
-    //         $('#notify .control .play').css('display','inline-block');
-    //         $('#notify .slick').slick('slickPause');
-    //     } else if ($(this).hasClass('play')) {
-    //         $(this).hide();
-    //         $('#notify .control .pause').css('display','inline-block');
-    //         $('#notify .slick').slick('slickPlay');
-    //     }
-    // });
-
-    // $('#business .slick').slick({
-    //     autoplay: false,
-    //     arrows: true,
-    //     dots: false,
-    //     prevArrow: $('#business .prev'),
-    //     nextArrow: $('#business .next'),
-    //     accessibility: false,
-    //     swipeToSlide: true,
-    //     draggable: true,
-    //     infinite: true,
-    //     slidesToShow: 3,
-    //     slidesToScroll: 1,
-    //     pauseOnHover: false,
-    //     speed: 500,
-    //     responsive: [{
-    //         breakpoint: 721,
-    //         settings: {
-    //             slidesToShow: 2,
-    //         }
-    //     }]
-    // });
-
-
-
+	
+    $('#main-visual-bx .serach_link .link_item').on('mouseover', function (e){
+        e.preventDefault();
+        $(this).parents('.serach_link').find('.link_item').removeClass('active');
+        $(this).addClass('active');
+    });
 });
 
