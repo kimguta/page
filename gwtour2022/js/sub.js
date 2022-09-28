a6f4fb1823012c42e41f15a7669e1e24b0320900
dq <dq@idq.co.kr>
2022-09-28 15:21:06 +0900

    서브 수정

--- gwtour2022/js/sub.js
+++ gwtour2022/js/sub.js
@@ -2,54 +2,6 @@
 //서브공통 스크립트
 var ObjDoc = $(document);		
 
-// ObjDoc.on({
-// 	'click': function(e) { 
-// 		e.preventDefault();
-// 		if ($(this).hasClass('active')) {
-// 			$(this).removeClass('active');
-// 			$(this).children('span').text('열기');
-// 			$('#breadcrumb-bx .box ul').slideUp(250);
-// 		} else{
-// 			$('#breadcrumb-bx .open').removeClass('active');
-// 			$(this).addClass('active');
-// 			$('#breadcrumb-bx .open span').text('열기');
-// 			$(this).children('span').text('닫기');
-// 			$('#breadcrumb-bx .box ul').slideUp(250);
-// 			$(this).next('ul').slideDown(250);
-// 		}
-// 	}
-// }, '#breadcrumb-bx .bx .open')
-// .on({
-// 	'mouseleave': function() { 
-// 		$('#breadcrumb-bx .open span').text('열기');
-// 		$('#breadcrumb-bx .box ul').slideUp(300);
-// 		$('#breadcrumb-bx .box .open').removeClass('active');
-// 	}
-// }, '#breadcrumb-bx')
-// .on({
-// 	'focusout': function() { 
-// 		$(this).parents('.box').children('.open').focus();
-// 	}
-// }, '#breadcrumb-bx .box ul li:last-child a')
-// .on({
-// 	'click': function(e) { 
-// 		e.preventDefault();
-// 		$(this).next('.view').fadeToggle(100);
-// 	}
-// }, '#title-bx .share .open')
-// .on({
-// 	'click': function(e) { 
-// 		e.preventDefault();
-// 		$(this).parents('.view').fadeToggle(100);
-// 	}
-// }, '#title-bx .share .close')
-// .on({
-// 	'focusout': function() { 
-// 		$('#title-bx .share .open').focus();
-// 		$('.view').fadeOut(100);
-// 	}
-// }, '#title-bx .share .view a:last');
-
 ObjDoc.on({
 	'click': function(e) { 
 		e.preventDefault();
