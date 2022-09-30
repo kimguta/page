71f1bbb925b8b97ced1d112bd4b5dd2fc8f909ec
dq <dq@idq.co.kr>
2022-09-30 17:44:14 +0900

    강원도청 작업 진행

---
+++ gwprovin/js/index.js
@@ -0,0 +1,40 @@
+$(function() { 
+    var slickOption = {
+        autoplay: true,
+        arrows: true,
+        accessibility: false,
+        dots:false,
+        prevArrow: $('#visual .prev'),
+        nextArrow: $('#visual .next'),
+        draggable: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        speed: 1200,
+        autoplaySpeed: 5000,
+    };
+    
+    initSlick($('#visual .slick'), slickOption);      
+});
+
+
+ObjDoc.on({
+	'click focusout': function(e) { 
+        e.preventDefault();
+		$('#player1')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+        $('#video-wrap').hide();
+        $('#main .video-on').focus();
+	}
+}, '#video-wrap .close')
+.on({
+	'click': function(e) { 
+        e.preventDefault();
+        $('#video-wrap').css('display','flex');
+        $('#player1')[0].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
+        $('#video-wrap a:first').focus();
+	}
+}, '#main .video-on');
+
+
+
