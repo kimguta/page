2d81ce307b69ee0e68979f92c4b0ea20ab15781a
guta <shimjunseop001@gmail.com>
2022-11-14 17:41:27 +0900

    그린시티 포털 메인 작업 진행중

--- greencity/js/index.js
+++ greencity/js/index.js
@@ -24,23 +24,68 @@ $(function() {
 
     var slickOption2 = {
         autoplay: true,
-        arrows: true,
+        arrows: false,
         accessibility: false,
         dots:false,
-        prevArrow: $('#notice .prev'),
-        nextArrow: $('#notice .next'),
         draggable: true,
-        infinite: false,
+        infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         pauseOnHover: false,
-        speed: 1200,
-        autoplaySpeed: 5000,
+        vertical: true,
+        verticalSwiping: true,
+        speed: 500,
+        autoplaySpeed: 6000,
+    };
+    
+    initSlick($('.vertical-bx .slick'), slickOption2);
+
+
+    var slickOption3 = {
+        autoplay: false,
+        arrows: true,
+        accessibility: false,
+        dots:false,
+        draggable: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        swipeToSlide:true,
+        speed: 350,
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
-                speed: 900,
+                centerMode: true,
+                slidesToShow: 3,
+                variableWidth: true,
+                }
+            }
+        ]
+    };
+    
+    initSlick($('.menu-bx .slick'), slickOption3);
+
+    var slickOption4 = {
+        autoplay: true,
+        arrows: true,
+        accessibility: false,
+        dots:false,
+        draggable: true,
+        prevArrow: $('.noti-bx .prev'),
+        nextArrow: $('.noti-bx .next'),
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        speed: 450,
+        autoplaySpeed: 6000,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                speed: 350,
                 centerMode: true,
                 slidesToShow: 3,
                 variableWidth: true,
@@ -50,13 +95,72 @@ $(function() {
         ]
     };
     
+    initSlick($('.noti-bx .slick'), slickOption4);
+
+
+    $('.edu-bx .slick').each( function() {
+        var prevBtn =  $(this).parent('.item').find('.prev');
+        var nextBtn =  $(this).parent('.item').find('.next');
+        var dotBox =  $(this).parent('.item').find('.dots');
+        $(this).slick({
+            autoplay: false,
+            arrows: true,
+            accessibility: false,
+            dots:true,
+            draggable: true,
+            prevArrow: prevBtn,
+            nextArrow: nextBtn,
+            appendDots: dotBox,
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            variableWidth: true,
+            swipeToSlide:true,
+            pauseOnHover: false,
+            speed: 450,
+        });
+      });
+
+
+      var slickOption5 = {
+        autoplay: false,
+        arrows: true,
+        accessibility: false,
+        dots:false,
+        draggable: true,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        speed: 700,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                speed: 500,
+                slidesToShow: 2,
+                variableWidth: true,
+                swipeToSlide:true,
+                }
+            }
+        ]
+    };
     
-    initSlick($('#notice .slick'), slickOption2);
-    
-    $('#visual .tab a:first, #visual .data-bx .item:first-child').addClass('active');
+    initSlick($('#vr-center .slick'), slickOption5);
+
+
+    $('#visual .tab a:first, #visual .data-bx .item:first-child, #education .tab a:first, #education .edu-bx .item:first-child, #board .tab a:first, #board .edu-bx .item:first-child').addClass('active');
 
     $('.data-bx > div').each(function (index, item) {
         $(item).find('.spot-bx a:first').addClass('active');
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 700,
+        delay: 500,
+        once: true,
+        offset: 50,   
     });
 
 });
@@ -92,16 +196,18 @@ ObjDoc.on({
         Item.find('.map-bx .spot-bx a').removeClass('active');
         Item.find('.map-bx .spot-bx a').eq(Idx2).addClass('active');
 	}
-}, '.local-bx .view a, .map-bx .spot-bx a');
-// .on({
-// 	'click': function(e) { 
-//         e.preventDefault();
-//         var Name2 = $(this).text();
-//         $(this).parents('.item').find('.open').text(Name2);
-//         $(this).parents('.item').find('.map-bx .spot-bx a').removeClass('active');
-//         $(this).addClass('active');
-// 	}
-// }, '.map-bx .spot-bx a')
+}, '.local-bx .view a, .map-bx .spot-bx a')
+.on({
+	'click': function(e) { 
+        e.preventDefault();
+        var Idx3 = $(this).index();
+        $(this).parent().find('a').removeClass('active');
+        $(this).addClass('active');
+        $(this).parent().next('div').find('.item').removeClass('active');
+        $(this).parent().next('div').find('.item').eq(Idx3).addClass('active');
+        $(this).parent().next('div').find('.item').eq(Idx3).find('.slick').slick('setPosition');
+	}
+}, '#education .tab a, #board .tab a');
 
 
 
