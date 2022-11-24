a194ea65088aef7b99153dc207fe590f09df588b
kimguta <shimjumseop001@gmail.com>
2022-11-24 21:06:36 +0900

    테스트

---
+++ gwtour-foreign/js/index.js
@@ -0,0 +1,208 @@
+$(function() { 
+    var sample = $('#Uplayer')[0];
+    $('.control-bx .play, .text-img').on('click',function(){
+        if ($('.control-bx .play').hasClass('active')) {
+            sample.pause();
+            $('.control-bx .play').removeClass('active');
+        } else{
+            sample.play();
+            $('.control-bx .play').addClass('active');
+            $('#visual .text-img').addClass('active');
+        }
+    });
+    $('.control-bx .sound').on('click',function(){
+        if ($(this).hasClass('active')) {
+            $('#Uplayer').prop('muted', true);
+            $(this).removeClass('active');
+        } else{
+            $('#Uplayer').prop('muted', false);
+            $(this).addClass('active');
+        }
+    });
+
+    $('#Uplayer').on("timeupdate", function() {
+        var video = $(this)[0];
+        var perc = 100 * video.currentTime / video.duration;
+        $('.progress span').css("width", perc + "%");
+        if (video.currentTime > 5) {
+            $('#visual').addClass('v1')
+        }
+        $('.video-wrap .point').each(function(index, item){
+            var sTime = $(this).data('start');
+            var eTime = $(this).data('end');
+            if (video.currentTime >= sTime && video.currentTime <= eTime) {
+                $(this).addClass('active');
+            }
+            else{
+                $(this).removeClass('active');
+            }
+        })
+    });
+
+    $('.video-wrap .point').on({
+        'mouseover': function() { 
+            sample.pause();
+            $('.video-wrap').addClass('active');
+        },
+        'mouseout': function() { 
+            sample.play();
+            $('.video-wrap').removeClass('active');
+        },
+    });
+
+    var windowWidth = $(window).width();
+    $('.video-wrap').on('mousemove', function(e) {
+        var moveX = (($(window).width() / 2) - e.pageX) * 0.4;
+        var moveY = (($(window).height() / 2) - e.pageY) * 0.6;
+        if (windowWidth > 1499) {
+            $('.video-wrap').stop().css('margin-left', moveX + 'px').css('margin-top', moveY + 'px');
+        }
+    });
+
+    var SlickOption1 = {
+        autoplay: false,
+        arrows: true,
+        accessibility: false,
+        dots:false,
+        swipeToSlide:true,
+        draggable: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        speed: 700,
+        responsive: [
+            {
+                breakpoint: 717,
+                settings: {
+                speed: 500,
+                }
+            }
+        ]
+    };
+
+    var SlickOption2 = {
+        autoplay: false,
+        arrows: false,
+        accessibility: false,
+        dots:false,
+        swipeToSlide:true,
+        draggable: true,
+        infinite: true,
+        lidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        speed: 1000,
+        asNavFor: '#map .slick2'
+    };
+
+    var SlickOption3 = {
+        autoplay: true,
+        arrows: true,
+        accessibility: false,
+        dots:false,
+        draggable: true,
+        infinite: true,
+        fade: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        speed: 800,
+        asNavFor: '#map .slick',
+        autoplaySpeed: 8000,
+    };
+
+    var SlickOption4 = {
+        autoplay: false,
+        arrows: false,
+        accessibility: false,
+        swipeToSlide:true,
+        variableWidth: true,
+        dots:false,
+        draggable: true,
+        infinite: true,
+        slidesToShow: 3,
+        pauseOnHover: false,
+        speed: 500,
+        responsive: [
+            {
+                breakpoint: 1500,
+                settings: {
+                speed: 350,
+                centerMode: true,
+                }
+            }
+        ]
+    };
+
+    initSlick($('#only .slick'), SlickOption1);
+    initSlick($('#map .slick'), SlickOption2);
+    initSlick($('#map .slick2'), SlickOption3);
+    initSlick($('#food .slick'), SlickOption4);    
+    // sideBar($('#like .tab-bx'), $('#like .tab-bx > div'));
+    $('#like .item-bx > div:first-child, #like .tab-bx a:first-of-type, #like .tab-bx p, #map .item-bx > div:first-child, #map .tab-bx a:first-child').addClass('active');
+});
+
+
+ObjDoc.on({
+	'click': function(e) { 
+		e.preventDefault();
+		var idx = $(this).index() - 1;
+        $('#like .item-bx > div, #like .tab-bx a').removeClass('active');
+        $('#like .item-bx > div').eq(idx).addClass('active');
+        $(this).addClass('active');
+        if ($(this).hasClass('tit2')) {
+            $('#like .tab-bx p').addClass('active');
+        } else{
+            $('#like .tab-bx p').removeClass('active');
+        }
+	}
+}, '#like .tab-bx a')
+.on({
+	'click': function(e) { 
+		e.preventDefault();
+		var idx = $(this).index();
+        $('#map .item-bx > div, #map .tab-bx a').removeClass('active');
+        $('#map .item-bx > div').eq(idx).addClass('active');
+        $(this).addClass('active');
+	}
+}, '#map .tab-bx a');
+
+
+
+ObjWin.on({
+	'scroll load': function() { 
+        if (Wwidth < 1500) {
+            $('.control-bx .play, .text-img').on('click',function(){
+                $('#visual').addClass('mobile');
+                var Sposition =  ($('#Uplayer').width() - Wwidth) / 2;
+                $('#visual .video-wrap').scrollLeft(Sposition);
+            });
+        }
+	}		
+});
+
+function sideBar(wrapElem, elem){
+    ObjWin.on('scroll', function(){
+            var posY = ObjWin.scrollTop() + 100;
+            var wOppset = wrapElem.offset();
+            var wOppsetB = wOppset.top + wrapElem.height() - 270;
+            if(wOppset.top < posY && wOppsetB > posY){
+                elem.css("top",posY - wOppset.top+"px").addClass('active');
+            }else if(wOppset.top > posY){
+                elem.css("top","0px").removeClass('active'); 
+            }
+            else{
+                elem.removeClass('active');    
+            }       
+	}); 
+}
+
+ObjWin.on('scroll load', function(){
+    var posY = ObjWin.scrollTop() + 300;
+    var welOppset = $('#welcome').prev().prev('section').offset().top;
+    if(welOppset < posY){
+        $('#welcome img').addClass('active');
+    }  
+});
+
