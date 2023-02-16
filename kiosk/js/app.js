2e6d45f38d09325dc8433d1f3d1ab86bbf024be9
guta <shimjunseop001@gmail.com>
2023-02-16 11:12:58 +0900

    지도 수정

--- kiosk/js/app.js
+++ kiosk/js/app.js
@@ -1,5 +1,16 @@
 
 $(function() {
-	
+    $('.slick-kiosk').slick({
+        autoplay: true,
+        arrows: false,
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        speed: 900,
+        autoplaySpeed: 15000,
+        cssEase: 'linear',
+    });
 });
 
