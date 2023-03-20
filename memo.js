72f8c93fcc226c8ee6e5bc27f190d96a494c234d
kimguta <70299722+kimguta@users.noreply.github.com>
2023-03-20 09:13:41 +0900

    Update memo.js

--- memo.js
+++ memo.js
@@ -1,6 +1,33 @@
 
 
 /*테스트*/
+//카운트 업 스크립트
+								function numberCounter(target_frame, target_number) {
+									this.count = 0; this.diff = 0;
+									this.target_count = parseInt(target_number);
+									this.target_frame = document.getElementById(target_frame);
+									this.timer = null;
+									this.counter();
+								};
+								numberCounter.prototype.counter = function() {
+									var self = this;
+									this.diff = this.target_count - this.count;
+									if(this.diff > 0) {
+										self.count += Math.ceil(this.diff / 5);
+									}
+									this.target_frame.innerHTML = this.count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+								
+									if(this.count < this.target_count) {
+										this.timer = setTimeout(function() { self.counter(); }, 30);
+									} else {
+										clearTimeout(this.timer);
+									}
+								};
+								new numberCounter("counter1", 489);
+								new numberCounter("counter2", 780);
+								new numberCounter("counter3", 18246);
+
+
 <div class="dday-ui">
 				<div class="dbx">
 					<img src="/main_cons/imgs/intro_logo_gwd.png" alt="강원특별자치도 출범">
