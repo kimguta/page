919f3ac8c9e3a980da8dfc2cd7ace15a6394f1c9
guta <shimjunseop001@gmail.com>
2025-11-07 14:30:23 +0900

    태백

--- tour/js/script.js
+++ tour/js/script.js
@@ -11,47 +11,47 @@ $(function() {
 	});
 
 
-	$('#gnb_pc .depth_01 h2 a').on('mouseenter focusin', function () {
-		$('#gnb_pc .depth_01 h2').removeClass('active');
-		$('#gnb_pc .depth_02 h3').removeClass('active');
-		$('.depth_02').removeClass('active');
-		$('#gnb_pc .depth_02').hide();
-		$('#gnb_pc .depth_03').hide();
-	    $(this).parent('h2').addClass('active');
-		$(this).parent('h2').next('.depth_02').stop().slideDown(300);
-		/*
-		$('.depth_02 > li:first-child h3').addClass('active');
-		$('.depth_02 > li:first-child .depth_03').show();
-		*/
-		$('.bg_pc').show().stop().animate({height:85},300);
-	});
-
-	$('#gnb_pc .depth_01 li:nth-child(4) h2 a').on('mouseenter focusin', function () {
-		$('.bg_pc').stop().animate({height:138},300);
-	});
-
+	// has_depth 표시
 	$('#gnb_pc .depth_03').closest('li').addClass('has_depth');
 
-	$('#gnb_pc .depth_02 h3 a').on('mouseover focusin', function () {
-	    $('#gnb_pc .depth_02 h3').removeClass('active');
-		$('#gnb_pc .depth_03').hide();
-		$(this).parent('h3').addClass('active');
-		$('.depth_02').removeClass('active');
-		$(this).parent('h3').next('.depth_03').show();
-		$('.bg_pc').show().stop().animate({height:85},300);	
+	// depth_01 진입
+	$('#gnb_pc .depth_01 h2 a').on('mouseenter focusin', function () {
+	// 초기화
+	$('#gnb_pc .depth_01 h2, #gnb_pc .depth_02 h3').removeClass('active');
+	$('#gnb_pc .depth_02, #gnb_pc .depth_03').hide();
+
+	// 현재 depth_02 오픈
+	var $h2 = $(this).parent('h2').addClass('active');
+	var $depth02 = $h2.next('.depth_02');
+
+	// slideDown이 끝난 뒤 실제 높이 계산 → bg에 반영
+	$depth02.stop(true, true).slideDown(0, function () {
+		var h = $depth02.outerHeight(true) + 50; // 여유값(패딩) 조절 가능
+		$('.bg_pc').show().stop(true, true).animate({ height: h }, 200);
+	});
 	});
 
-	$('#gnb_pc .depth_02 .has_depth h3 a').on('mouseover focusin', function () {
-		$('.bg_pc').show().stop().animate({height:122},300);
-	});
+	// depth_02 → depth_03 진입
+	$('#gnb_pc .depth_02 h3 a').on('mouseenter focusin', function () {
+	var $h3 = $(this).parent('h3');
 
-	$('#gnb_pc .depth_01 > li:nth-child(4) h3 a').on('mouseover focusin', function () {
-		$('.bg_pc').stop().animate({height:138},300);
+	// 상태 초기화/갱신
+	$('#gnb_pc .depth_02 h3').removeClass('active');
+	$('#gnb_pc .depth_03').hide();
+	$h3.addClass('active');
 
-	});
-
-	$('#gnb_pc .depth_01 > li:nth-child(4) .has_depth h3 a').on('mouseover focusin', function () {
-		$('.bg_pc').stop().animate({height:175},300);
+	// 해당 depth_03 표시 후 높이 측정
+	var $depth03 = $h3.next('.depth_03');
+	if ($depth03.length) {
+		$depth03.show();
+		var h3 = $depth03.outerHeight(true) + 75;
+		$('.bg_pc').show().stop(true, true).animate({ height: h3 }, 200);
+	} else {
+		// depth_03이 없는 h3면 현재 depth_02 높이 기준
+		var $depth02 = $h3.closest('.depth_02');
+		var h2 = $depth02.outerHeight(true) + 75;
+		$('.bg_pc').show().stop(true, true).animate({ height: h2 }, 200);
+	}
 	});
 
 	$('#gnb_pc .depth_02 > li:nth-child(3) h3 a').on('mouseover focusin', function () {
