479db62f3b7d2de453d69e4044d43bd859e383af
guta <shimjunseop001@gmail.com>
2026-04-16 13:55:45 +0900

    Deploy DQ site to GitHub Pages path

--- dq-src/vite.config.js
+++ dq-src/vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/page/dq/',
   plugins: [react()],
+  build: {
+    outDir: '../dq',
+    emptyOutDir: true,
+  },
 });
